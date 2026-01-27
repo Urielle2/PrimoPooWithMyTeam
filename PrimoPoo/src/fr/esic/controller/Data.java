9e0027dd046aba387e04b232517241f7ba1faf26
Ousmane Mbathie <ousmanembathie179@gmail.com>
2026-01-27 14:56:44 +0100

    maj data

--- PrimoPoo/src/fr/esic/controller/Data.java
+++ PrimoPoo/src/fr/esic/controller/Data.java
@@ -6,12 +6,12 @@ import fr.esic.model.Person;
 public class Data {
 	public static void init() {
 		Produit P1 = new Produit("Ordinateur Portable", "PC portable haute performance", 1200.00, 10);
-		Produit P2 = new Produit("Smartphone", "Téléphone intélligent ", 800.00, 10);
-		Produit P3 = new Produit("Casque audio", "Casque sans fil avec réduction de bruit", 150.00, 10);
-		Produit P4 = new Produit("Tablette", "Tablette légère pour la navigation et le multimédia", 400.00, 10);
+		Produit P2 = new Produit("Smartphone", "Téléphone intélligent ", 800.00, 12);
+		Produit P3 = new Produit("Casque audio", "Casque sans fil avec réduction de bruit", 150.00, 14);
+		Produit P4 = new Produit("Tablette", "Tablette légère pour la navigation et le multimédia", 400.00, 23);
 		Produit P5 = new Produit("Tablette IOS", "Tablette portable haute performance", 600.00, 10);
-		Produit P6 = new Produit("Tablette Android", "Tablette légère pour la navigation et le multimédia", 300.00, 10);
-		Produit P7 = new Produit("Ordinateur de Bureau", "PC de bureau puissant pour le gaming", 1500.00, 10);
+		Produit P6 = new Produit("Tablette Android", "Tablette légère pour la navigation et le multimédia", 300.00, 25);
+		Produit P7 = new Produit("Ordinateur de Bureau", "PC de bureau puissant pour le gaming", 1500.00, 18);
 		
 		
 		
